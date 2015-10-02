9b2ccd500b28a2b4fc7f16c4ca0bf1df6b81fe13
Mattias Giese <giese@b1-systems.de>
2015-10-02 09:43:08 +0200

    On the way to more customizability

--- manifests/director/fileset.pp
+++ manifests/director/fileset.pp
@@ -1,57 +1,74 @@
-# == Define: bareos::director::fileset
+# Define bareos::director::fileset
 #
-# Configure an additional basic file set to be used by the Bacula clients. See the {FileSet
-# Resource}[http://www.bareos.org/5.0.x-manuals/en/main/main/Configuring_Director.html#SECTION001870000000000000000] documentation
-# for information on the options.
-#
-# === Parameters
-#
-# [*ensure*]
-#   Ensure the file is present or absent.  The only valid values are <tt>file</tt> or
-#   <tt>absent+. Defaults to <tt>file</tt>.
-# [*exclude_files*]
-#   An array of strings consisting of one file or directory name per entry. Directory names should be specified without
-#   a trailing slash with Unix path notation.
-# [*include_files*]
-#   *Required*: An array of strings consisting of one file or directory name per entry. Directory names should be specified without
-#   a trailing slash with Unix path notation.
-#
-# === Examples
-#
-#   $servicename_include_files = [
-#     '/etc/servicename',
-#     '/var/lib/servicename',
-#     '/var/log/servicename',
-#     '/var/spool/servicename',
-#   ]
-#   $servicename_exclude_files = [
-#     '/var/lib/servicename/tmp'
-#   ]
-#
-#   bareos::director::fileset { 'servicename' :
-#     ensure         => file,
-#     include_files => $servicename_include_files,
-#     exclude_files => $servicename_exclude_files,
-#   }
+# Used to create filesets resources
 #
 define bareos::director::fileset (
-  $ensure   = 'file',
-  $exclude_files = undef,
-  $include_files = undef
+  $ensure                 = 'file',
+  $signature              = 'MD5',
+  $compression            = 'GZIP',
+  $onefs                  = '',
+  $fstype                 = '',
+  $recurse                = '',
+  $sparse                 = '',
+  $noatime                = '',
+  $mtimeonly              = '',
+  $keepatime              = '',
+  $checkfilechanges       = '',
+  $hardlinks              = '',
+  $ignorecase             = '',
+  $include                = '',
+  $exclude                = '',
+  $ignore_fileset_changes = '',
+  $options_hash           = {},
+  $restart                = true,
+  $template               = 'bareos/director/fileset.conf.erb'
 ) {
 
-  if !($ensure in ['file', 'absent']) {
-    fail('The only valid values for the ensure parameter are file or absent')
+  include ::bareos::params
+
+  $array_filesets_fstype = is_array($fstype) ? {
+    false     => $fstype ? {
+      ''      => [],
+      default => [$fstype],
+    },
+    default   => $fstype,
   }
 
-  file { "/etc/bareos/bareos-dir.d/fileset-${name}.conf":
-    ensure  => file,
-    owner   => 'bareos',
-    group   => 'bareos',
-    mode    => '0640',
-    content => template('bareos/fileset.conf.erb'),
-    require => File['/etc/bareos/bareos-dir.conf'],
-    before  => Service['bareos-dir'],
-    notify  => Exec['bareos-dir reload'],
+  $array_filesets_include = is_array($include) ? {
+    false     => $include ? {
+      ''      => [],
+      default => [$include],
+    },
+    default   => $include,
   }
+
+  $array_filesets_exclude = is_array($exclude) ? {
+    false     => $exclude ? {
+      ''      => [],
+      default => [$exclude],
+    },
+    default   => $exclude,
+  }
+
+  $manage_fileset_file_content = $template ? {
+    ''      => undef,
+    default => template($template),
+  }
+
+  $fileset_conf = "${bareos::params::director_confd}/fileset-${name}.conf"
+
+  file { $fileset_conf:
+    ensure  => $ensure,
+    mode    => $::bareos::params::config_file_mode,
+    owner   => $::bareos::params::config_owner,
+    group   => $::bareos::params::config_group,
+    require => Package[$::bareos::params::director_packages],
+    content => $manage_fileset_file_content,
+  }
+
+  if $restart {
+    File[$fileset_conf] ~> Service[$bareos::params::director_service]
+  }
+
 }
+
